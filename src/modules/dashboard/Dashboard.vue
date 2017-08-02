e91899f8c53ee323732594dc1f35f74d522c6edd
kuratongbaleleng <insuteppukikku1@gmail.com>
2017-08-02 14:54:38 +0800

    Update Dashboard.vue and route file
    updated Dashboard.vu and dev_routes/salise.js

---
+++ src/modules/dashboard/Dashboard.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+	<div align="center">
+	welcome
+	<table>
+	<tr>
+
+	<bars :data="[50, 10, 40, 10,5,6,6,6,6,6]"
+		  :gradient="['#6fa8dc', '#42b983']"
+		  :growDuration="1">
+	</bars>
+	</tr>
+	<tr>
+	</tr>
+ </table>
+ </div>
+ </div>
+</template>
+<script>
+	import Vue from 'vue'
+	import Bars from 'vuebars'
+
+	Vue.use(Bars)
+
+</script>
+<style scoped>
+	
+</style>
